c3cace18477824e0b90b11646d4532add76a2069
farizian <farizirfan8@gmail.com>
2021-07-09 08:08:06 +0700

    first commit

---
+++ tugas1/post app/js/data.js
@@ -0,0 +1,58 @@
+const product =[
+    {
+        id: 1,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/espresso.png",
+        product_name: "Espresso",
+        price: `Rp. ${10000}`,
+    },
+    {
+        id: 2,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/cofeelatte.png",
+        product_name: "Coffe Latte",
+        price: `Rp. ${15000}`,
+    },
+    {
+        id: 3,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/cappucinos.png",
+        product_name: "Cappucino",
+        price: `Rp. ${5000}`,
+    },
+    {
+        id: 4,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/redvelvet.png",
+        product_name: "Red Velvet Latte",
+        price: `Rp. ${33000}`,
+    },
+    {
+        id: 5,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/chocorum.png",
+        product_name: "Choco Rum",
+        price: `Rp. ${28000}`,
+    },
+    {
+        id: 6,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/blackforest.png",
+        product_name: "Black Forest",
+        price: `Rp. ${30000}`,
+    },
+    {
+        id: 7,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/chickenkatsu.png",
+        product_name: "Chicken Katsu",
+        price: `Rp. ${60000}`,
+    },
+    {
+        id: 8,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/salmon.png",
+        product_name: "Salmon Truffle",
+        price: `Rp. ${60000}`,
+    },
+    {
+        id: 9,
+        picture: "https://raw.githubusercontent.com/farizian/week10/master/tugas1/img/wiener.png",
+        product_name: "Wiener Schnitzel",
+        price: `Rp. ${69000}`,
+    },
+]
+
+const cart =[]
